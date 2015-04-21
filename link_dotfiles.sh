79434331e4b6e40657f27b5124bc9a0c5fe8e573
momota <makoto.momota@gmail.com>
2015-04-21 11:39:33 +0900

    add making synlink for git

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -2,3 +2,5 @@
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.gitignore ~/.gitignore
