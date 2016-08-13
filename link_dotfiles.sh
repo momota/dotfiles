b5ac5f0f6054a3d8f1f21937def40a41f8b67bf7
momota <makoto.momota@gmail.com>
2016-08-14 00:29:16 +0900

    [add]create .vimperatorrc

--- link_dotfiles.sh
+++ link_dotfiles.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.vimperatorrc ~/.vimperatorrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.gitignore ~/.gitignore
